15271b718404004e9801812decafcf8254f41897
fraander <57777918+fraander@users.noreply.github.com>
2024-06-11 11:12:38 -0400

    Initial Commit

---
+++ Lastly/LastlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastlyApp.swift
+//  Lastly
+//
+//  Created by Frank Anderson on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LastlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
